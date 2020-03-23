20d777154376714cef82a2d28ec4dbc5f2a23884
dulceanumaria <48507106+dulceanumaria@users.noreply.github.com>
2020-03-23 15:50:46 +0200

    Add files via upload

--- ModelDesignFirst_L1/Model1.Context.cs
+++ ModelDesignFirst_L1/Model1.Context.cs
@@ -12,71 +12,73 @@ namespace ModelDesignFirst_L1
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
-
+    
     public partial class Model1Container : DbContext
     {
         public Model1Container()
             : base("name=Model1Container")
         {
         }
-
+    
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
         }
-
+    
         public virtual DbSet<Person> People { get; set; }
-    }
-    public partial class ModelOneToManyContainer : DbContext
-    {
-        public ModelOneToManyContainer()
-        : base("name=ModelOneToManyContainer")
-        { }
 
-        protected override void OnModelCreating(
-       DbModelBuilder modelBuilder)
+
+        public partial class ModelOneToManyContainer : DbContext
         {
-            throw new UnintentionalCodeFirstException();
-        }
+            public ModelOneToManyContainer()
+            : base("name=ModelOneToManyContainer")
+            { }
 
-        public virtual DbSet<Customer> Customers { get; set; }
-        public virtual DbSet<Order> Orders { get; set; }
-
-        static void TesTOneToMany()
-        {
-            Console.WriteLine("One to many association");
-            using (ModelOneToManyContainer context =
-           new ModelOneToManyContainer())
+            protected override void OnModelCreating(
+           DbModelBuilder modelBuilder)
             {
-                Customer c = new Customer()
+                throw new UnintentionalCodeFirstException();
+            }
+
+            public virtual DbSet<Customer> Customers { get; set; }
+            public virtual DbSet<Order> Orders { get; set; }
+
+            static void TesTOneToMany()
+            {
+                Console.WriteLine("One to many association");
+                using (ModelOneToManyContainer context =
+               new ModelOneToManyContainer())
                 {
-                    Name = "Customer 1",
-                    City = "Iasi"
-                };
-                Order o1 = new Order()
-                {
-                    TotalValue = 200,
-                    Date = DateTime.Now,
-                    Customer = c
-                };
-                Order o2 = new Order()
-                {
-                    TotalValue = 300,
-                    Date = DateTime.Now,
-                    Customer = c
-                };
-                context.Customers.Add(c);
-                context.Orders.Add(o1);
-                context.Orders.Add(o2);
-                context.SaveChanges();
-                var items = context.Customers;
-                foreach (var x in items)
-                {
-                    Console.WriteLine("Customer : {0}, {1}, {2}",
-                   x.CustomerId, x.Name, x.City);
-                    foreach (var ox in x.Orders)
-                        Console.WriteLine("\tOrders: {0}, {1}, {2}",
-                       ox.OrderId, ox.Date, ox.TotalValue);
+                    Customer c = new Customer()
+                    {
+                        Name = "Customer 1",
+                        City = "Iasi"
+                    };
+                    Order o1 = new Order()
+                    {
+                        TotalValue = 200,
+                        Date = DateTime.Now,
+                        Customer = c
+                    };
+                    Order o2 = new Order()
+                    {
+                        TotalValue = 300,
+                        Date = DateTime.Now,
+                        Customer = c
+                    };
+                    context.Customers.Add(c);
+                    context.Orders.Add(o1);
+                    context.Orders.Add(o2);
+                    context.SaveChanges();
+                    var items = context.Customers;
+                    foreach (var x in items)
+                    {
+                        Console.WriteLine("Customer : {0}, {1}, {2}",
+                       x.CustomerId, x.Name, x.City);
+                        foreach (var ox in x.Orders)
+                            Console.WriteLine("\tOrders: {0}, {1}, {2}",
+                           ox.OrderId, ox.Date, ox.TotalValue);
+                    }
                 }
             }
         }
