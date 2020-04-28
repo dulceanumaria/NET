f9cd1b4622e33aabed0e0c962ba4f49e4c720b12
dulceanumaria <48507106+dulceanumaria@users.noreply.github.com>
2020-04-28 16:01:57 +0300

    Add files via upload

---
+++ gRPC_si_EF/GrpcGreeterClient/Interface1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ServiceModel;
+using PostComment;
+
+namespace GrpcGreeterClient
+{
+    interface InterfacePost
+    {
+        bool AddPost(Post post);
+        Post UpdatePost(Post post);
+        int DeletePost(int id);
+        Post GetPostById(int id);
+        List<Post> GetPosts();
+    }
+    interface InterfaceComment
+    {
+        bool AddComment(Comment comment);
+        Comment UpdateComment(Comment newComment);
+        Comment GetCommentById(int id);
+    }
+    interface IPostComment : InterfacePost, InterfaceComment
+    {
+    }
+
+}
