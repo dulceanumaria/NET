ab7a889fc2f29d125e07b326c988b3c2cdd24c62
dulceanumaria <48507106+dulceanumaria@users.noreply.github.com>
2020-04-28 18:28:33 +0300

    Add files via upload

---
+++ gRPC_si_EF/PostComment/Model1.edmx.sql
@@ -0,0 +1,84 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 03/24/2020 14:22:55
+-- Generated from EDMX file: C:\Users\marina\source\repos\PostComment\Model1.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [master];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Comments'
+CREATE TABLE [dbo].[Comments] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Text] nvarchar(50)  NOT NULL,
+    [PostPostId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Posts'
+CREATE TABLE [dbo].[Posts] (
+    [PostId] int IDENTITY(1,1) NOT NULL,
+    [Description] nvarchar(50)  NOT NULL,
+    [Domain] nvarchar(50)  NOT NULL,
+    [Date] datetime  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Comments'
+ALTER TABLE [dbo].[Comments]
+ADD CONSTRAINT [PK_Comments]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [PostId] in table 'Posts'
+ALTER TABLE [dbo].[Posts]
+ADD CONSTRAINT [PK_Posts]
+    PRIMARY KEY CLUSTERED ([PostId] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [PostPostId] in table 'Comments'
+ALTER TABLE [dbo].[Comments]
+ADD CONSTRAINT [FK_PostComment]
+    FOREIGN KEY ([PostPostId])
+    REFERENCES [dbo].[Posts]
+        ([PostId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_PostComment'
+CREATE INDEX [IX_FK_PostComment]
+ON [dbo].[Comments]
+    ([PostPostId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
