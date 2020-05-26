d6a3e1ca8864a9bdbbbb4f5bde40220d696f125e
dulceanumaria <48507106+dulceanumaria@users.noreply.github.com>
2020-05-26 15:04:08 +0300

    Add files via upload

---
+++ Proiect3/WebApplication/Pages/Files/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceReference;
+
+namespace WebApplication.Pages.Files
+{
+    public class CreateModel : PageModel
+    {
+        InterfaceWCFClient pcc = new InterfaceWCFClient();
+
+        [BindProperty]
+        public FilesDTO FilesDTO { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            FilesDTO file = new FilesDTO();
+            file.cale = FilesDTO.cale;
+            file.nume_fisier = FilesDTO.nume_fisier;
+            file.data_creare = FilesDTO.data_creare;
+            file.locatie = FilesDTO.locatie;
+            file.eveniment = FilesDTO.eveniment;
+            file.peisaj = FilesDTO.peisaj;
+            file.descriere = FilesDTO.descriere;
+            await pcc.AddFileAsync(file.nume_fisier, file.cale, file.data_creare, file.locatie, file.eveniment, file.peisaj, file.descriere);
+            
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- Proiect3/WebApplication/Pages/Files/Index.cshtml.cs
+++ Proiect3/WebApplication/Pages/Files/Index.cshtml.cs
@@ -6,12 +6,19 @@ using WebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ServiceReference;
+
 namespace WebApplication.Pages.Files
 {
     public class IndexModel : PageModel
     {
         InterfaceWCFClient pcc = new InterfaceWCFClient();
         public List<FilesDTO> Files { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchPeisaj { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchEveniment { get; set; }
 
         public IndexModel()
         {
@@ -19,40 +26,160 @@ namespace WebApplication.Pages.Files
         }
         public async Task OnGetAsync()
         {
-            var files = await pcc.GetFilesAsync("da", "da", "da", "da", "da");
-            foreach (var item in files)
+            if (string.IsNullOrEmpty(SearchString) && string.IsNullOrEmpty(SearchPeisaj) && string.IsNullOrEmpty(SearchEveniment))
             {
-                // Trebuia folosit AutoMapper. Transform Post in PostDTO
-                FilesDTO pd = new FilesDTO();
-                pd.Id = item.Id;
-                pd.nume_fisier = item.nume_fisier;
-                pd.cale = item.cale;
-                pd.data_creare = item.data_creare;
-                pd.locatie = item.locatie;
-                pd.eveniment = item.eveniment;
-                pd.peisaj = item.peisaj;
-                pd.descriere = item.descriere;
-                pd.del = item.del;
-                foreach (var cc in item.Persoanes)
+                var files = await pcc.GetAllFilesAsync();
+                foreach (var item in files)
                 {
-                    PersoaneDTO cdto = new PersoaneDTO();
-                    cdto.Id = cc.Id;
-                    cdto.nume_persoana = cc.nume_persoana;
-                    cdto.del = cc.del;
-                    pd.Persoanes.Add(cdto);
+                    // Trebuia folosit AutoMapper. Transform Post in PostDTO
+                    FilesDTO pd = new FilesDTO();
+                    pd.Id = item.Id;
+                    pd.nume_fisier = item.nume_fisier;
+                    pd.cale = item.cale;
+                    pd.data_creare = item.data_creare;
+                    pd.locatie = item.locatie;
+                    pd.eveniment = item.eveniment;
+                    pd.peisaj = item.peisaj;
+                    pd.descriere = item.descriere;
+                    pd.del = item.del;
+                    foreach (var cc in item.Persoanes)
+                    {
+                        PersoaneDTO cdto = new PersoaneDTO();
+                        cdto.Id = cc.Id;
+                        cdto.nume_persoana = cc.nume_persoana;
+                        cdto.del = cc.del;
+                        pd.Persoanes.Add(cdto);
+                    }
+                    foreach (var cc in item.Useris)
+                    {
+                        UseriDTO cdto = new UseriDTO();
+                        cdto.Id = cc.Id;
+                        cdto.nume_complet = cc.nume_complet;
+                        cdto.mail = cc.mail;
+                        cdto.parola = cc.parola;
+                        cdto.del = cc.del;
+                        pd.Useris.Add(cdto);
+                    }
+
+                    Files.Add(pd);
                 }
-                foreach (var cc in item.Useris)
+
+            }
+            else if (!string.IsNullOrEmpty(SearchString) && string.IsNullOrEmpty(SearchPeisaj) && string.IsNullOrEmpty(SearchEveniment))
+            {
+                var files = await pcc.GetFilesByLocatieAsync(SearchString);
+                foreach (var item in files)
                 {
-                    UseriDTO cdto = new UseriDTO();
-                    cdto.Id = cc.Id;
-                    cdto.nume_complet = cc.nume_complet;
-                    cdto.mail = cc.mail;
-                    cdto.parola = cc.parola;
-                    cdto.del = cc.del;
-                    pd.Useris.Add(cdto);
+                    // Trebuia folosit AutoMapper. Transform Post in PostDTO
+                    FilesDTO pd = new FilesDTO();
+                    pd.Id = item.Id;
+                    pd.nume_fisier = item.nume_fisier;
+                    pd.cale = item.cale;
+                    pd.data_creare = item.data_creare;
+                    pd.locatie = item.locatie;
+                    pd.eveniment = item.eveniment;
+                    pd.peisaj = item.peisaj;
+                    pd.descriere = item.descriere;
+                    pd.del = item.del;
+                    foreach (var cc in item.Persoanes)
+                    {
+                        PersoaneDTO cdto = new PersoaneDTO();
+                        cdto.Id = cc.Id;
+                        cdto.nume_persoana = cc.nume_persoana;
+                        cdto.del = cc.del;
+                        pd.Persoanes.Add(cdto);
+                    }
+                    foreach (var cc in item.Useris)
+                    {
+                        UseriDTO cdto = new UseriDTO();
+                        cdto.Id = cc.Id;
+                        cdto.nume_complet = cc.nume_complet;
+                        cdto.mail = cc.mail;
+                        cdto.parola = cc.parola;
+                        cdto.del = cc.del;
+                        pd.Useris.Add(cdto);
+                    }
+
+                    Files.Add(pd);
                 }
-                Files.Add(pd);
+            }
+            else if (string.IsNullOrEmpty(SearchString) && !string.IsNullOrEmpty(SearchPeisaj) && string.IsNullOrEmpty(SearchEveniment))
+            {
+                var files = await pcc.GetFilesByPeisajAsync(SearchPeisaj);
+                foreach (var item in files)
+                {
+                    // Trebuia folosit AutoMapper. Transform Post in PostDTO
+                    FilesDTO pd = new FilesDTO();
+                    pd.Id = item.Id;
+                    pd.nume_fisier = item.nume_fisier;
+                    pd.cale = item.cale;
+                    pd.data_creare = item.data_creare;
+                    pd.locatie = item.locatie;
+                    pd.eveniment = item.eveniment;
+                    pd.peisaj = item.peisaj;
+                    pd.descriere = item.descriere;
+                    pd.del = item.del;
+                    foreach (var cc in item.Persoanes)
+                    {
+                        PersoaneDTO cdto = new PersoaneDTO();
+                        cdto.Id = cc.Id;
+                        cdto.nume_persoana = cc.nume_persoana;
+                        cdto.del = cc.del;
+                        pd.Persoanes.Add(cdto);
+                    }
+                    foreach (var cc in item.Useris)
+                    {
+                        UseriDTO cdto = new UseriDTO();
+                        cdto.Id = cc.Id;
+                        cdto.nume_complet = cc.nume_complet;
+                        cdto.mail = cc.mail;
+                        cdto.parola = cc.parola;
+                        cdto.del = cc.del;
+                        pd.Useris.Add(cdto);
+                    }
+
+                    Files.Add(pd);
+                }
+            }
+            else if (string.IsNullOrEmpty(SearchString) && string.IsNullOrEmpty(SearchPeisaj) && !string.IsNullOrEmpty(SearchEveniment))
+            {
+                var files = await pcc.GetFilesByEvenimentAsync(SearchEveniment);
+                foreach (var item in files)
+                {
+                    // Trebuia folosit AutoMapper. Transform Post in PostDTO
+                    FilesDTO pd = new FilesDTO();
+                    pd.Id = item.Id;
+                    pd.nume_fisier = item.nume_fisier;
+                    pd.cale = item.cale;
+                    pd.data_creare = item.data_creare;
+                    pd.locatie = item.locatie;
+                    pd.eveniment = item.eveniment;
+                    pd.peisaj = item.peisaj;
+                    pd.descriere = item.descriere;
+                    pd.del = item.del;
+                    foreach (var cc in item.Persoanes)
+                    {
+                        PersoaneDTO cdto = new PersoaneDTO();
+                        cdto.Id = cc.Id;
+                        cdto.nume_persoana = cc.nume_persoana;
+                        cdto.del = cc.del;
+                        pd.Persoanes.Add(cdto);
+                    }
+                    foreach (var cc in item.Useris)
+                    {
+                        UseriDTO cdto = new UseriDTO();
+                        cdto.Id = cc.Id;
+                        cdto.nume_complet = cc.nume_complet;
+                        cdto.mail = cc.mail;
+                        cdto.parola = cc.parola;
+                        cdto.del = cc.del;
+                        pd.Useris.Add(cdto);
+                    }
+
+                    Files.Add(pd);
+                }
             }
         }
     }
-}
+    }
+
